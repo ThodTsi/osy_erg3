16958a4e7db46975bec38efc9b1f4c4d74b66fdb
Styliani04 <moumtzistella0@gmail.com>
2024-01-02 00:33:44 +0200

    oy mpoi

--- ergasia3.s
+++ ergasia3.s
@@ -8,23 +8,27 @@
     main:
         
         main_loop:
-            jal read_option #op=readOption();
-            sw $v0,op
-            lw $t0,op
+                jal read_option #op=readOption();
+                sw $v0,op
+                lw $t0,op
 
-            blt $t0,1,exit  #elegxos while
-            bgt $t0,8,exit
+                blt $t0,1,exit  #elegxos while
+                bgt $t0,8,exit
 
             case_1:
-                bne $t0,1,case_2
+                bne $t0,1,case_2    #if(op!=1)
 
                 la $a0,c1   #printStr
                 li $v0,4
                 syscall
 
+                la $a0,pinA  #readPin(pinA)
+                jal read_pin
+                move $t1,$a0
+
                 j read_op
             case_2:
-                bne $t0,2,case_3
+                bne $t0,2,case_3    #if(op!=2)
 
                 la $a0,c2   #printStr
                 li $v0,4
@@ -32,7 +36,7 @@
 
                 j read_op
             case_3:
-                bne $t0,3,case_4
+                bne $t0,3,case_4    #if(op!=3)
 
                 la $a0,c3   #printStr
                 li $v0,4
@@ -40,7 +44,7 @@
 
                 j read_op
             case_4:
-                bne $t0,4,case_5
+                bne $t0,4,case_5    #if(op!=4)
 
                 la $a0,c4   #printStr
                 li $v0,4
@@ -48,7 +52,7 @@
 
                 j read_op
             case_5:
-                bne $t0,5,case_6
+                bne $t0,5,case_6    #if(op!=5)
 
                 la $a0,c5   #printStr
                 li $v0,4
@@ -56,7 +60,7 @@
 
                 j read_op
             case_6:
-                bne $t0,6,case_7
+                bne $t0,6,case_7    #if(op!=6)
 
                 la $a0,c6   #printStr
                 li $v0,4
@@ -64,7 +68,7 @@
 
                 j read_op
             case_7:
-                bne $t0,7,case_8
+                bne $t0,7,case_8    #if(op!=7)
 
                 la $a0,c7   #printStr
                 li $v0,4
@@ -72,83 +76,117 @@
 
                 j read_op
             case_8:
-                bne $t0,8,read_op
+                bne $t0,8,read_op   #if(op!=8)
 
                 la $a0,c8   #printStr
                 li $v0,4
                 syscall
 
                 j read_op
-
-
-            read_op:
+        read_op:
                 j main_loop
+        exit:
+                la $a0,w    #printStr
+                li $v0,4
+                syscall
 
-
-    exit:
-        la $a0,w    #printStr
-        li $v0,4
-        syscall
-
-        li $v0,10   #exit
-        syscall
+                li $v0,10   #exit
+                syscall
 
     read_option:
     #-----------printing choices--------------------------------------------------
-        la $a0,line
-        li $v0,4
-        syscall
+            la $a0,line
+            li $v0,4
+            syscall
 
-        la $a0,d1
-        li $v0,4
-        syscall
+            la $a0,d1
+            li $v0,4
+            syscall
 
-        la $a0,d2
-        li $v0,4
-        syscall
+            la $a0,d2
+            li $v0,4
+            syscall
 
-        la $a0,d3
-        li $v0,4
-        syscall
+            la $a0,d3
+            li $v0,4
+            syscall
 
-        la $a0,d4
-        li $v0,4
-        syscall
+            la $a0,d4
+            li $v0,4
+            syscall
 
-        la $a0,d5
-        li $v0,4
-        syscall
+            la $a0,d5
+            li $v0,4
+            syscall
 
-        la $a0,d6
-        li $v0,4
-        syscall
+            la $a0,d6
+            li $v0,4
+            syscall
 
-        la $a0,d7
-        li $v0,4
-        syscall
+            la $a0,d7
+            li $v0,4
+            syscall
 
-        la $a0,d8
-        li $v0,4
-        syscall
+            la $a0,d8
+            li $v0,4
+            syscall
 
-        la $a0,d0
-        li $v0,4
-        syscall
+            la $a0,d0
+            li $v0,4
+            syscall
 
-        la $a0,line
-        li $v0,4
-        syscall
+            la $a0,line
+            li $v0,4
+            syscall
 
-        la $a0,ch
-        li $v0,4
-        syscall
+            la $a0,ch
+            li $v0,4
+            syscall
 
-        li $v0,5
-        syscall
+            li $v0,5
+            syscall
 
-        move $v0,$v0    #back to main
-        jr $ra
+            move $v0,$v0    #back to main
+            jr $ra
+
+    read_pin:
+            li $s0,0 #counter i
+            lw $s2,pinlen
+            move $s1,$a0    #$s1=pinA (base register)
+            
+        loop:
+            bge $s0,$s2,end_r   #if(i>=pin.length)
+
+            la $a0,pos  #printStr
+            li $v0,4
+            syscall
+
+            move $a0,$s0    #printInt
+            li $v0,1
+            syscall
+
+            la $a0,sem  #printStr
+            li $v0,4
+            syscall 
+             
+            li $v0,5    #readInt
+            syscall
+
+            sb $v0,($s1)    #pin[i] = nextInt
+            add $s1,$s1,1   #nextInt
+            add $s0,$s0,1   #i++
+            j loop  
+        end_r:
+            move $a0,$s1    #back to main
+            jr $ra
         
+    test:
+        la $a0,line
+        li $v0,4
+        syscall
+
+        jr $ra
+
                
 
 .data
@@ -173,3 +211,8 @@
     d0: .asciiz "0, Exit \n"
     line: .asciiz "\n-----------------------------\n"
     ch: .asciiz "Choice? \n"
+    pinA: .space 40
+    pinB: .space 40
+    pinlen: .word 10
+    pos: .asciiz "Position "
+    sem: .asciiz ": "
